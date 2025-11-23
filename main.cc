7ba5e9f34b343ef99a365c124cef9e6b6e947991
Henri-Thinkpad <henriqueirineu10@hotmail.com>
2025-11-23 15:59:52 -0300

    replace single quotes with double quptes

--- main.cc
+++ main.cc
@@ -9,7 +9,7 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
-    std::clog << '\rScanlines remaining: ' << (image_height - j) << ' '
+    std::clog << "\r Scanlines remaining: " << (image_height - j) << " "
               << std::flush;
 
     for (int i = 0; i < image_width; i++) {
